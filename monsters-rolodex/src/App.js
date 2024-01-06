f0bbdbcf50d3756a28f210ac770f11fc48445566
Maxime Mondini <mh-mondini@hotmail.com>
2024-01-05 23:43:20 -0500

    fix minor bug

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -36,7 +36,7 @@ const App = () => {
     <div className='App'>
       <h1 className='app-title'>Monsters Rolodex</h1>
       <SearchBox
-        className='monsters-search-box'
+        className={'search-box'}
         onChangeHandler={onSearchChange}
         placeholder='search monsters'
       />
